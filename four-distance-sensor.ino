8d153414452ccb43f749e682d3150676f6165f67
nejdetkadir <nejdetkadir.550@gmail.com>
2020-10-02 20:02:28 +0300

    Add led bulb and turn on the light

--- four-distance-sensor.ino
+++ four-distance-sensor.ino
@@ -1,11 +1,16 @@
 #define first_trig 3
 #define first_echo 2
+#define first_led 53
 #define second_trig 5
 #define second_echo 4
+#define second_led 51
 #define third_trig 7
 #define third_echo 6
+#define third_led 49
 #define fourth_trig 9
 #define fourth_echo 8
+#define fourth_led 47
+
 
 void setup() {
   //OUTPUTS
@@ -13,6 +18,7 @@ void setup() {
   pinMode(second_trig, OUTPUT);
   pinMode(third_trig, OUTPUT);
   pinMode(fourth_trig, OUTPUT); 
+  pinMode(first_led, OUTPUT);
 
   //INPUTS
   pinMode(first_echo, INPUT);
@@ -28,12 +34,21 @@ void loop() {
   String order[] = {"first", "second", "third", "fourth"};
   int max_distance = arr[0];
   int max_order = 0;
+  int leds[] = {first_led, second_led, third_led, fourth_led};
   for(int i=0; i<4; i++) {
     if (max_distance < arr[i]) {
       max_distance = arr[i];
       max_order = i;
     }
   }
+
+  for(int i=0; i<4; i++) {
+    if (i == max_order) {
+      digitalWrite(leds[i], HIGH);
+    } else {
+      digitalWrite(leds[i], LOW);
+    }
+  }  
 
   Serial.print("The most distant sensor : ");
   Serial.print(order[max_order]);
