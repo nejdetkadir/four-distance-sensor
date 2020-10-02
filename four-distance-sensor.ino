5b9c07afa024a5ac206da84143dd13129e846a48
nejdetkadir <nejdetkadir.550@gmail.com>
2020-10-02 19:09:07 +0300

    Add files via upload

---
+++ four-distance-sensor.ino
@@ -0,0 +1,54 @@
+#define first_trig 3
+#define first_echo 2
+#define second_trig 5
+#define second_echo 4
+#define third_trig 7
+#define third_echo 6
+#define fourth_trig 9
+#define fourth_echo 8
+
+void setup() {
+  //OUTPUTS
+  pinMode(first_trig, OUTPUT);
+  pinMode(second_trig, OUTPUT);
+  pinMode(third_trig, OUTPUT);
+  pinMode(fourth_trig, OUTPUT); 
+
+  //INPUTS
+  pinMode(first_echo, INPUT);
+  pinMode(second_echo, INPUT);
+  pinMode(third_echo, INPUT);
+  pinMode(fourth_echo, INPUT); 
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  int arr[] = {calculateDistance(first_echo, first_trig), calculateDistance(second_echo, second_trig), calculateDistance(third_echo, third_trig), calculateDistance(fourth_echo, fourth_trig)};
+  String order[] = {"first", "second", "third", "fourth"};
+  int max_distance = arr[0];
+  int max_order = 1;
+  for(int i=0; i<4; i++) {
+    if (max_distance < arr[i]) {
+      max_distance = arr[i];
+      max_order = i+1;
+    }
+  }
+
+  Serial.print("en uzak ölçüm yapan cihaz ");
+  Serial.print(max_order);
+  Serial.print("\n");
+}
+
+int calculateDistance(int echo, int trigger) {
+  int sure, mesafe;
+
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigger, LOW);
+
+  sure = pulseIn(echo, HIGH);
+  mesafe = (sure/2) / 29.1;
+  
+  return mesafe;  
+}
